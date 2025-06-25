9daf35db50401bb22c9c4dfe33119a40b9ed3e04
MiguelVivar <miguelvivarfarfan@gmail.com>
2025-06-25 17:29:39 -0500

    feat: Add SQL script for creating microservices databases and update Docker configuration

---
+++ db/init.sql
@@ -0,0 +1,7 @@
+-- Crear las bases de datos para los microservicios
+CREATE DATABASE autenticacion;
+CREATE DATABASE intranet;
+CREATE DATABASE matricula;
+
+-- Verificar que las bases de datos se crearon
+\l
